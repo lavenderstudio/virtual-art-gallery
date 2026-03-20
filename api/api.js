52f86bcc054ee58fb036d16d41c82d16b014a35c
lavenderstudio <craftstudiovietnam@gmail.com>
2026-03-20 14:58:19 +0700

    Update default export and add met.js require
    Changed default export value from 'artic' to 'met' and added require for met.js.

--- api/api.js
+++ api/api.js
@@ -1,7 +1,10 @@
-'strict mode';
+'use strict';
 
 module.exports = {
-    default: "artic",
+    // Đổi default thành "met"
+    default: "met",
+    // Thêm dòng require file met.js vừa tạo
+    met: require("./met"),
     artic: require("./artic"),
     local: require("./local")
 };

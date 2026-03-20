c379965f37ed6a9e59de956609a912bceb56a10c
lavenderstudio <craftstudiovietnam@gmail.com>
2026-03-20 18:29:27 +0700

    Update image.js

--- src/image.js
+++ src/image.js
@@ -1,9 +1,7 @@
 'use strict';
 
-// Sửa lại đường dẫn để budo tìm thấy thư mục api bên trong src
-const api = require('./api/index.js'); 
-const selectedApi = new URLSearchParams(window.location.search).get("api");
-const dataAccess = api[selectedApi] || api[api.default];
+// GỌI TRỰC TIẾP FILE MET.JS NẰM CÙNG THƯ MỤC SRC
+const dataAccess = require('./met.js'); 
 const text = require('./text');
 
 let paintingCache = {};
@@ -26,57 +24,36 @@ async function loadImage(regl, url) {
 			regl._gl.getExtension('EXT_texture_filter_anisotropic').MAX_TEXTURE_MAX_ANISOTROPY_EXT
 		) : 0;
 	}
-	
 	try {
 		return new Promise((resolve, reject) => {
 			const img = new Image();
-			// Khắc phục lỗi đen màn hình bằng cách cho phép tải ảnh từ Met Museum
 			img.crossOrigin = "anonymous"; 
 			img.src = url;
 			img.onload = () => {
 				ctx.drawImage(img, 0, 0, resizeCanvas.width, resizeCanvas.height);
 				const tex = (unusedTextures.pop() || regl.texture)({
 					data: resizeCanvas,
-					min: 'mipmap',
-					mipmap: 'nice',
-					aniso,
-					flipY: true
+					min: 'mipmap', mipmap: 'nice', aniso, flipY: true
 				});
-				resolve([
-					tex,
-					width => text.init((unusedTextures.pop() || regl.texture), "Lavender Prime Art", width),
-					img.width / img.height
-				]);
+				resolve([tex, width => text.init((unusedTextures.pop() || regl.texture), "Lavender Prime", width), img.width / img.height]);
 			};
-			img.onerror = () => {
-				console.error("Không tải được ảnh:", url);
-				resolve(emptyImage(regl));
-			};
+			img.onerror = () => resolve(emptyImage(regl));
 		});
-	} catch(e) {
-		return emptyImage(regl);
-	}
+	} catch(e) { return emptyImage(regl); }
 }
 
 module.exports = {
 	fetch: (regl, count = 10, res = "low", cbOne, cbAll) => {
-		// dataAccess chính là hàm async từ met.js
 		dataAccess().then(urls => {
 			if (!urls || urls.length === 0) return cbAll();
 			let remaining = urls.length;
-			
 			urls.forEach((url) => {
 				loadImage(regl, url).then(([tex, textGen, aspect]) => {
 					cbOne({ image_id: url, tex, textGen, aspect });
 					if (--remaining === 0) cbAll();
-				}).catch(() => {
-					if (--remaining === 0) cbAll();
-				});
+				}).catch(() => { if (--remaining === 0) cbAll(); });
 			});
-		}).catch(err => {
-			console.error("Lỗi API:", err);
-			cbAll();
-		});
+		}).catch(() => cbAll());
 	},
 	load: (regl, p, res = "low") => {
 		if (p.tex || p.loading) return;
